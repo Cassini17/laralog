c702689e215ca4364d35d19e4d8e791c822f509a
Fred Hou <fred@FreddeAir.hub>
2017-10-31 11:24:27 +1300

    test

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -4,19 +4,13 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script>
- function throwOut(JSON){
-  console.log(JSON);
- }
- $(document).ready(function(){
-  $.ajax({
-   url: "ip-api.com/json",
-   dataType: "jsonp",
-   jsonpCallback: "throwOut",
-   success: function(rtn){ $("#test").html( JSON.stringify(rtn) ); },
-   error: function(err){ $("#test").html( " statusText = " + err.statusText + " status = " + err.status); }
-  });
- });
- 
+        $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+            var table_body = "";
+            $.each(data, function(k, v) {
+                table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
+            });
+            $("#GeoResults").html(table_body);
+        });
 </script>
-<span id="test">hello</span>
+<table id="GeoResults"></table>
 </html>
