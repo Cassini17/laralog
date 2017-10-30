822a3c2b1d1922b163c74186edc17e87694ca801
Fred Hou <fred@FreddeAir.hub>
2017-10-31 00:53:11 +1300

    for test

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,123 +1,22 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Jenny and her friends</title>
-
-    <!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	
-    <!-- Custom styles for this template -->
-    <link href="/css/app.css" rel="stylesheet">
-</head>
-<body>
-		
-	<div class="blog-masthead">
-      <div class="container">
-        <nav class="nav blog-nav">
-          <a class="nav-link active" href="/">Blog</a>
-          <a class="nav-link" href="#">Study</a>
-          <a class="nav-link" href="/memos">Memos</a>
-                    
-          <li class="ml-auto dropdown">
-          	<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Fred Hou</a>
-          	<ul class="dropdown-menu">
-          		<li><a class="dropdown-item" href="/posts/create">New post</a></li>
-          		<li><a class="dropdown-item" href="/memos/create">New memo</a></li>
-          		<li><a class="dropdown-item" href="/profile">Profile</a></li>
-          		<li><a class="dropdown-item" href="">Settings</a></li>
-          		<li>
-                    <a class="dropdown-item" href="http://zoudiqiu.com/logout" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Sign out
-                    </a>
-                    <form id="logout-form" action="http://zoudiqiu.com/logout" method="POST" style="dis
-play: none;">
-                        <input type="hidden" name="_token" value="aVgnQwc5oNMyp7gbzTwzHSOCT8gvwODco35s0VnG">
-                    </form>
-                </li>
-          	</ul>
-          </li>
-                  </nav>
-      </div>
-    </div>
-
-    <div class="blog-header">
-      <div class="container">
-        <h1 class="blog-title">Jenny And Family</h1>
-        <p class="lead blog-description">They live happily in the beautiful world.</p>
-      </div>
-    </div>	
-    <div class="container">
-
-      <div class="row">
-          
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Datepicker - Default functionality</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/css/datepicker-style.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  </script>
-
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script>
+ function throwOut(JSON){
+  console.log(JSON);
+ }
+ $(document).ready(function(){
+  $.ajax({
+   url: "ip-api.com/json",
+   //dataType: "jsonp",
+   //jsonpCallback: "throwOut",
+   success: function(rtn){ $("#test").html( JSON.stringify(rtn) ); },
+   error: function(err){ $("#test").html( " statusText = " + err.statusText + " status = " + err.status); }
+  });
+ });
  
-<p>Date: <input type="text" id="datepicker"></p>
- 
- 
-		<div class="col-sm-3 offset-sm-1 blog-sidebar">
-          <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-          <div class="sidebar-module">
-            <h4>Archives</h4>
-            <ol class="list-unstyled">
-                        </ol>
-          </div>
-           <div class="sidebar-module">
-            <h4>Tags</h4>
-            <ol class="list-unstyled">
-                        </ol>
-          </div>
-          <div class="sidebar-module">
-            <h4>Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-            </ol>
-          </div>
-        </div><!-- /.blog-sidebar -->
-      </div><!-- /.row -->
-
-
-    </div><!-- /.container -->
-
-	<footer class="blog-footer">
-      <p>Designed and built with all the love in the world by Jenny's father - Fred @2017.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-    
-    
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-
-</body>
+</script>
+<span id="test">hello</span>
 </html>
